a1f801fa4ef4135a83c4b0e7fcdc9231bf42207f
Christopher.Bonetto <christopherbonetto@gmail.com>
2019-10-05 19:11:33 +0200

    chris_testscene added to move the player

---
+++ Assets/Developer/Chris/PlayerActions.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerActions : MonoBehaviour
+{
+    [SerializeField] private LayerMask m_obstacleLayer;
+    [SerializeField] private float m_raycastDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckFrontPlayer();
+    }
+
+    public void CheckFrontPlayer()
+    {
+        RaycastHit hit;
+        Vector3 blockNormal;
+
+        if (Physics.Raycast(gameObject.transform.position, Vector3.right, out hit, m_raycastDistance, m_obstacleLayer))
+        {
+            blockNormal = hit.normal;
+            Debug.DrawRay(hit.point, blockNormal, Color.blue, 100f);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Debug.DrawRay(gameObject.transform.position, Vector3.right * m_raycastDistance, Color.red);
+    }
+}
