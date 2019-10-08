3ffb347ff01ad12940dccb9ab950f276011f5248
AlessandroPantano <alessandro.pantano@outlook.it>
2019-10-08 12:24:34 +0200

    Update UI fade system

--- Assets/Developer/Ale/Windows/FadeBetweenScene.cs
+++ Assets/Developer/Ale/Windows/FadeBetweenScene.cs
@@ -20,7 +20,6 @@ public class FadeBetweenScene : UIControl
         set { m_OnFadeOutComplete = value; }
     }
 
-
     private Animator m_anim;
 
     protected override void Awake()
@@ -29,17 +28,24 @@ public class FadeBetweenScene : UIControl
         m_anim = GetComponent<Animator>();
     }
 
+    /// <summary>
+    /// Called in animation event
+    /// </summary>
     public void OnFadeInCompleted()
     {
-        // delegate, store action
+        // execute a method putted in (when fade in is completed)
         OnFadeInComplete?.Invoke();
+        OnFadeInComplete = null;
 
         m_anim.SetBool("isSceneLoaded", true);
     }
 
-    // Change this with a delegate.
+    /// <summary>
+    /// Called in animation event
+    /// </summary>
     public void OnFadeOutCompleted()
     {
         OnFadeOutComplete?.Invoke();
+        OnFadeOutComplete = null;
     }
 }
--- Assets/Developer/Ale/Windows/GameWindow.cs
+++ Assets/Developer/Ale/Windows/GameWindow.cs
@@ -5,4 +5,62 @@ using UnityEngine;
 public class GameWindow : UIControl
 {
     public override UIControlName Name => UIControlName.InGame;
+
+
+    /// <summary>
+    /// Called when click home button
+    /// </summary>
+    public void OnHomeButton()
+    {
+        // Store fade class
+        FadeBetweenScene fade = UIManager.Instance.Controls[UIControlName.Fade] as FadeBetweenScene;
+
+        #region Local Method
+        // Show level panel.
+        void ShowLevel()
+        {
+            UIManager.Instance.Show(UIControlName.LevelSelection);
+        }
+
+        // Turn off fade panel
+        void HideFade()
+        {
+            UIManager.Instance.Hide(UIControlName.Fade);
+        }
+        #endregion
+
+        // Store into delegate.
+        fade.OnFadeOutComplete = HideFade;
+        fade.OnFadeInComplete = ShowLevel;
+
+        // Turn on fade panel and disable this.
+        UIManager.Instance.ShowAndHide(UIControlName.Fade, this);
+    }
+
+    /// <summary>
+    /// Called when a level is completed.
+    /// </summary>
+    public void OnLevelCompleted()
+    {
+        // Store fade class
+        FadeBetweenScene fade = UIManager.Instance.Controls[UIControlName.Fade] as FadeBetweenScene;
+
+        #region Local Method
+        // Load new Level
+        // public void LoadLevel
+  
+        // Turn off fade and enable game window.
+        void ShowAndHideGameAndFade()
+        {
+            UIManager.Instance.ShowAndHide(this.Name, fade);
+        }
+        #endregion
+
+        // Store into delegate.
+        //fade.OnFadeInComplete = LoadLevel;
+        fade.OnFadeOutComplete = ShowAndHideGameAndFade;
+
+        // Turn on fade panel and disable this.
+        UIManager.Instance.ShowAndHide(UIControlName.Fade, this);
+    }
 }
