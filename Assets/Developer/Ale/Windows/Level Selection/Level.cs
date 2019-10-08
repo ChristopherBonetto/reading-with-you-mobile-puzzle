f45c14e6a90aefdcc39acae345ce725820cf7e20
AlessandroPantano <alessandro.pantano@outlook.it>
2019-10-08 15:38:52 +0200

    Update Level script

---
+++ Assets/Developer/Ale/Windows/Level Selection/Level.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Level name", menuName = "PangoBlocks/Level")]
+public class Level : ScriptableObject
+{
+    #region Inspector
+    [Header("UI field")]
+    public Sprite Icon;
+
+    [Header("Level")] [Tooltip("Drag here the entire level prefab")]
+    public GameObject LevelPrefab;
+
+    [Tooltip("All necessary block to complete the level")]
+    public GameObject[] BlocksToPlace;
+    #endregion
+}
