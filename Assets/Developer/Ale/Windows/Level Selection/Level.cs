9c8df21312d2bab5e6534e37388de177199c9b7a
AlessandroPantano <alessandro.pantano@outlook.it>
2019-10-11 15:10:34 +0200

    Update World and Level connection

---
+++ Assets/Developer/Ale/Windows/Level Selection/Level.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "PangoBlecks/Level", fileName = "Name Level")]
+public class Level : ScriptableObject
+{
+    public Sprite Icon;
+    public Sprite IconLocked;
+
+    public GameObject LevelPrefab;
+
+    public int Index { get; set; }
+}
--- Assets/Developer/Ale/Windows/Level Selection/World.cs
+++ Assets/Developer/Ale/Windows/Level Selection/World.cs
@@ -1,16 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[System.Serializable]
-public class Level
-{
-    public Sprite Icon;
-
-    private LevelSelectionWindow m_levelSelection => UIManager.Instance.Controls[UIControlName.LevelSelection] as LevelSelectionWindow;
-
-    public int Index { get; set; }
-}
-
 [CreateAssetMenu(fileName = "World name", menuName = "PangoBlocks/UI/World")]
 public class World : ScriptableObject
 {
