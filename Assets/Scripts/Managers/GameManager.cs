b1326dd1ce1b673f5766ba9d8da744ee34f77d3c
Christopher.Bonetto <christopherbonetto@gmail.com>
2019-10-25 11:55:49 +0200

    Reorganization folders - final object updated - particles updates

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using UnityEngine;
+
+public enum GameState
+{
+    Menu = 0,
+    Loading = 1,
+    Playing = 2,
+    Moving = 3
+}
+
+/// <summary>
+/// Difficult of the game.
+/// </summary>
+public enum Mode
+{
+    Easy,
+    Hard,
+}
+
+public class GameManager : Singleton<GameManager>
+{
+	public PlayerActions Player;
+
+	public FinalObjectActions FinalObject;
+
+	public World[] Worlds;
+
+	private Level m_currentLevelInfo;
+	public int m_currentLevel { get; private set; }
+	public int CurrentWorld { get; set; }
+	private GameObject m_currentMap;
+
+	/// <summary>
+	/// Event on player movement start
+	/// </summary>
+	public Action OnMovement;
+
+    /// <summary>
+    /// Event on load level
+    /// </summary>
+    public Action OnUpdateLevel;
+
+	private GameState m_currentState;
+    private Mode m_Mode;
+
+	public GameState CurrentState => m_currentState;
+    public Mode Mode => m_Mode;
+
+	private void Start()
+	{
+        //@TEMP @ALE
+        for (int i = 0; i < Worlds.Length; i++)
+        {
+            Worlds[i].EasyLevels[0].IsPlayable = true;
+            Worlds[i].HardLevels[0].IsPlayable = true;
+
+			for (int j = 0; j < Worlds[i].EasyLevels.Length; j++)
+			{
+				PoolableObject map = Worlds[i].EasyLevels[j].LevelToPool;
+				if (map)
+				{
+					ObjectPooler.Instance.AddPoolItem(map, 1, false); 
+				}
+			}
+
+			for (int j = 0; j < Worlds[i].HardLevels.Length; j++)
+			{
+				PoolableObject map = Worlds[i].HardLevels[j].LevelToPool;
+				if (map)
+				{
+					ObjectPooler.Instance.AddPoolItem(map, 1, false);
+				}
+			}
+		}
+
+		ObjectPooler.Instance.StartPooling();
+	}
+
+	public void SetGameState(GameState inGameState)
+    {
+        if (m_currentState == inGameState)
+        {
+            return;
+        }
+
+        m_currentState = inGameState;
+    }
+
+    public void SetMode(Mode inMode)
+    {
+        if (m_Mode == inMode)
+        {
+            return;
+        }
+
+        m_Mode = inMode;
+    }
+
+    public void StartWalkingPlayer()
+    {
+        Player.EnableMovement(true);
+        SetGameState(GameState.Moving);
+        OnMovement?.Invoke();
+	}
+
+	public void LoadLevel(int levelNo)
+	{
+		LoadLevel(CurrentWorld, levelNo);
+	}
+
+	private void LoadLevel(int worldNo, int levelNo)
+	{
+		// Unload current level
+		if (m_currentMap)
+		{
+			m_currentMap.SetActive(false);
+			BlockManager.Instance.UnloadBlocks();
+		}
+
+		// Load level
+		CurrentWorld = worldNo;
+		m_currentLevel = levelNo;
+		int levelID = GetLevelID();
+		if (levelID >= 0)
+		{
+			//@TODO Handle load level animation
+			m_currentMap = ObjectPooler.Instance.GetPooledObject(levelID);
+			if (m_currentMap)
+			{
+				m_currentMap.SetActive(true);
+			}
+			else
+			{
+				Debug.LogWarning("Can't unpool map. ID " + levelID + " not found.");
+			}
+			Player.ResetLevel(m_currentLevelInfo.PlayerCoords);
+			FinalObject.ResetLevel(m_currentLevelInfo.GoalObject.Coords, m_currentLevelInfo.Icon);
+			BlockManager.Instance.LoadBlocks(m_currentLevelInfo.Blocks);
+			//@TODO Set objective
+			SetGameState(GameState.Playing);
+		}
+		else
+		{
+			Debug.Log("Level number out of bounds");
+		}
+	}
+
+	private int GetLevelID()
+	{
+        if (Mode == Mode.Easy)
+        {
+		    if (CurrentWorld < Worlds.Length && m_currentLevel < Worlds[CurrentWorld].EasyLevels.Length)
+		    {
+			    m_currentLevelInfo = Worlds[CurrentWorld].EasyLevels[m_currentLevel];
+			    return m_currentLevelInfo.LevelID;
+		    }
+		    else
+		    {
+			    return -1;
+		    }
+        }
+
+        else if (Mode == Mode.Hard)
+        {
+            if (CurrentWorld < Worlds.Length && m_currentLevel < Worlds[CurrentWorld].HardLevels.Length)
+            {
+                m_currentLevelInfo = Worlds[CurrentWorld].HardLevels[m_currentLevel];
+                return m_currentLevelInfo.LevelID;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        return -1;
+	}
+
+	public void EndLevel(bool bWin)
+	{
+		if (bWin)
+		{
+            if (Mode == Mode.Easy)
+            {
+			    // Load next level
+			    if (m_currentLevel < Worlds[CurrentWorld].EasyLevels.Length - 1)
+			    {
+				    LoadLevel(m_currentLevel + 1);
+                }
+			    else if (CurrentWorld < Worlds.Length - 1)
+			    {
+				    //@TODO Handle end world animations
+				    LoadLevel(CurrentWorld + 1, 0);
+			    }
+            }
+
+            else if (Mode == Mode.Hard)
+            {
+                // Load next level
+                if (m_currentLevel < Worlds[CurrentWorld].HardLevels.Length - 1)
+                {
+                    LoadLevel(m_currentLevel + 1);
+                }
+                else if (CurrentWorld < Worlds.Length - 1)
+                {
+                    //@TODO Handle end world animations
+                    LoadLevel(CurrentWorld + 1, 0);
+                }
+            }
+        }
+		else
+		{
+			//@TODO Handle reload animation
+			Player.ResetLevel(m_currentLevelInfo.PlayerCoords);
+            FinalObject.EnableDisableCollider(true);
+			SetGameState(GameState.Playing);
+		}
+	}
+}
---
+++ Assets/Scripts/Managers/TouchManager.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+
+public class TouchManager : Singleton<TouchManager>
+{
+    #region Variables
+
+	private bool m_isHolding;
+
+    #endregion
+
+    #region Core loop
+
+	void Update()
+	{
+		if (GameManager.Instance.CurrentState != GameState.Playing)
+		{
+			return;
+		}
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit testHit))
+			{
+				Block testBlock = testHit.collider.GetComponent<Block>();
+				if (testBlock)
+				{
+					m_isHolding = true;
+					BlockManager.Instance.StartDrag(testBlock);
+				}
+				else
+				{
+					Tap(testHit);
+				}
+			}
+		}
+
+		if (Input.GetMouseButton(0) && m_isHolding)
+		{
+			BlockManager.Instance.Move();
+		}
+
+		if (Input.GetMouseButtonUp(0) && m_isHolding)
+		{
+			BlockManager.Instance.Release();
+			m_isHolding = false;
+		}
+	}
+
+	#endregion
+
+	#region Touch
+
+	private void Tap(RaycastHit hitted)
+	{
+        if(hitted.transform.GetComponent<PlayerActions>() || hitted.transform.GetComponent<FinalObjectActions>())
+        {
+            if (CanStart())
+            {
+                GameManager.Instance.StartWalkingPlayer();
+                GameManager.Instance.FinalObject.EnableDisableCollider(false);
+            }
+        }
+	}
+
+	private bool CanStart()
+	{
+		return (!m_isHolding &&
+				GameManager.Instance.CurrentState != GameState.Moving &&
+				BlockManager.Instance.UnstableBlocks == 0);
+	}
+
+	#endregion
+}
