63a7b04deb24fbb11201e3bdcb2a591e1d76e050
AlessandroPantano <alessandro.pantano@outlook.it>
2019-10-26 13:13:47 +0200

    Add base class for save system

---
+++ Assets/Scripts/Profiles/SaveSystem.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class SaveSystem
+{
+    public static void Save()
+    {
+        try
+        {
+            // Create new file or existed file
+        }
+        catch
+        {
+            // catch errors
+        }
+        finally
+        {
+            // close the file.
+        }
+    }
+
+    public static void Load()
+    {
+        try
+        {
+            // Open the file
+        }
+        catch
+        {
+            // catch errors
+        }
+        finally
+        {
+            // close the file.
+        }
+    }
+}
