484c039364616e82465ecbfd74bd1330a8b9e530
AlessandroPantano <alessandro.pantano@outlook.it>
2019-10-29 17:04:43 +0100

    Add text info about world and level @TEMP

---
+++ Assets/Scripts/UI/LevelInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelInfo : MonoBehaviour
+{
+    public Text Text;
+
+    private void OnEnable()
+    {
+        Text.text = "WORLD: " + GameManager.Instance.CurrentWorld.ToString() + " | LEVEL: " + GameManager.Instance.m_currentLevel.ToString();
+    }
+    //private void OnDisable()
+    //{
+    //    Text.text = "WORLD: " + GameManager.Instance.CurrentWorld.ToString() + "| LEVEL: " + GameManager.Instance.m_currentLevel.ToString();
+        
+    //}
+}
