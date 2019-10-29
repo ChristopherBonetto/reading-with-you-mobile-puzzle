66a5fb62e53f2aa4e61ff521bc3ea1c1f58d9ea7
Paibotm <>
2019-10-29 17:44:18 +0100

    Add white objective icons

--- Assets/Scripts/FinalObject/FinalObjectActions.cs
+++ Assets/Scripts/FinalObject/FinalObjectActions.cs
@@ -35,7 +35,6 @@ public class FinalObjectActions : MonoBehaviour
         EnableEndLevelParticle();
 		m_floater.transform.position = new Vector3(m_objectIcon.transform.position.x, m_objectIcon.transform.position.y + 1.5f, m_objectIcon.transform.position.z);
 		m_floater.enabled = false;
-		//m_floater.ResetPosition();
     }
 
 
