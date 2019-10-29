a2ad0b0622831670d2faee1e1f0971d7d8192870
Paibotm <>
2019-10-29 17:14:04 +0100

    Fix level info instance reference

--- Assets/Scripts/UI/LevelInfo.cs
+++ Assets/Scripts/UI/LevelInfo.cs
@@ -1,19 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class LevelInfo : MonoBehaviour
 {
     public Text Text;
 
-    private void OnEnable()
-    {
-        Text.text = "WORLD: " + GameManager.Instance.CurrentWorld.ToString() + " | LEVEL: " + GameManager.Instance.m_currentLevel.ToString();
-    }
-    //private void OnDisable()
-    //{
-    //    Text.text = "WORLD: " + GameManager.Instance.CurrentWorld.ToString() + "| LEVEL: " + GameManager.Instance.m_currentLevel.ToString();
-        
-    //}
+	private void OnEnable()
+	{
+		if (GameManager.Instance)
+		{
+			Text.text = "WORLD: " + GameManager.Instance.CurrentWorld.ToString() + " | LEVEL: " + GameManager.Instance.m_currentLevel.ToString(); 
+		}
+	}
 }
